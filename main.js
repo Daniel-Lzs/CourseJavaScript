9ae5ad41c3e294fe6145510447cb499a30453e57
Daniel-Lzs <dan201619@gmail.com>
2024-06-10 17:13:44 -0300

    feat: add 2 age

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let firstName = prompt('Digite o seu primeiro nome: ')
 let lastName = prompt('Digite o seu segundo nome: ')
-let age = prompt('Digite a sua idade: ')
+let age = parseInt(prompt('Digite a sua idade: '))
+const age2 = 2
+
+age += age2
 
 console.log(firstName)
 console.log(lastName)
