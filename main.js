9e97c4c8b38fada9ae8d434d3afb37d5b78050ee
Daniel-Lzs <dan201619@gmail.com>
2024-06-11 15:32:11 -0300

    feat: list of friends

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-let firstName = prompt('Insira o seu primeiro nome: ')
-console.log(firstName)
-greetings = 'Hello'
-document.getElementById('greetings').innerHTML = greetings + ' ' + firstName
+// let friends = ['Adriel', 'Gabriel', 'Marcos', 'Jonas']
+let friends = ['Alan', 'Mark', 'Julia', 'Bella']
+console.log(friends)
+friends[1] = 'Anna' // replace
+friends[4] = 'Maverick' // insert
+friends[5] = 1 // insert
+console.log(friends)
+console.log(friends[1])
