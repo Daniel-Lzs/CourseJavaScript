6238f9f621ccc99672f44727a80f44a63157530c
Daniel-Lzs <dan201619@gmail.com>
2024-06-12 15:33:29 -0300

    feat: content list with html

--- main.js
+++
@@ -1,9 +0,0 @@
-let valorConta = parseFloat(prompt('Digite o valor da conta: '))
-let gorjeta = parseFloat(prompt('Digite o valor da gorjeta: '))
-
-// gorjeta = (valorConta * gorjeta) /100
-// let valorTotal = valorConta + gorjeta
-
-let valorTotal = valorConta + (valorConta * gorjeta)/100
-
-alert('O valor total da gorjeta é de $: ' + valorTotal)
