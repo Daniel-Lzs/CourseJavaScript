7505d0f3f0e7737c3245782f092d68fea1f0a49b
Daniel-Lzs <dan201619@gmail.com>
2024-06-12 13:12:10 -0300

    feat: student grade calculation

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-let celsius = parseFloat(prompt('Digite o número em celsius: '))
-let fahrenheit = (celsius * 1.8) + 32
-alert('A temperatura em farenheit é de: ' + fahrenheit.toFixed(2))
+let nota = parseFloat(prompt('Insira a sua nota: '))
+
+if (nota >= 90 && nota <= 100){
+    alert('Você tirou a nota A')
+}
+else if(nota >= 80 && nota <= 89){
+    alert('Você tirou a nota B')
+}
+else if(nota >= 70 && nota <= 79){
+    alert('Você tirou a nota C')
+}
+else if(nota >= 60 && nota <= 69){
+    alert('Você tirou a nota D')
+}
+else if (nota > 100 || nota < 0){
+    alert('A nota inserida é inválida')
+}
+else{
+    alert('Você tirou a nota F')
+}
