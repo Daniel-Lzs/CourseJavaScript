13778722c1519a0b687a0f2bdfc243fa8c99b83e
Daniel-Lzs <dan201619@gmail.com>
2024-06-13 10:46:37 -0300

    feat: color list

---
+++ main.js
@@ -0,0 +1,9 @@
+let colorList = []
+
+for (let i = 1; i <= 3; i++){
+    let userColor = prompt('Digite a cor ' + i + ' : ' )
+    colorList.push(userColor)
+    // colorList[i] = userColor // Primeiro indice vazio
+}
+document.getElementById('listacores').innerHTML = colorList
+// alert('Sua lista de cores ficou assim: ' + colorList)
